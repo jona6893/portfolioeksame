84803036e1fe9e86374d6d2586e35ce79d8b6f04
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-01-06 00:39:23 +0100

    gået i gang med tema layout

--- header/header.js
+++ header/header.js
@@ -1,4 +1,5 @@
 //Variabler
+
 const menuIcon = document.querySelector(".menuIcon");
 const menuIcon2 = document.querySelector(".menuIcon2");
 const slidermenu = document.querySelector(".slidermenu");
@@ -9,16 +10,19 @@ const slidermenu = document.querySelector(".slidermenu");
 // Husk at skrive kommentar//
 menuIcon.addEventListener("click", () => {
     console.log("hambuger")
-    slidermenu.classList.toggle("hidded");
+    slidermenu.classList.toggle("skjult");
+    slidermenu.classList.toggle("tilsyne");
   menuIcon.classList.toggle("gone");
   menuIcon2.classList.toggle("gone");
+
 
 });
 
 
 menuIcon2.addEventListener("click", () => {
     console.log("kryds");
-    slidermenu.classList.toggle("hidded");
+    slidermenu.classList.toggle("skjult");
+    slidermenu.classList.toggle("tilsyne");
     menuIcon.classList.toggle("gone");
     menuIcon2.classList.toggle("gone");
 
