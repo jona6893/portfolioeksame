fcf040683be721abc6154ba34bd7b4950e2987cb
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-01-07 17:14:18 +0100

    smalll changes

---
+++ projekter/temaer/tema2.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => indicateScrollBar());
+
+function indicateScrollBar() {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.documentElement.style.setProperty("--scroll", `${scrolled}%`);
+}
